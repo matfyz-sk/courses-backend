77232e0761b7bd5d686e723cabd3e22832e25cc2
Milan Cifra <milan3362@gmail.com>
2020-04-26 15:26:07 +0200

    response json

--- src/middleware/responseHandler.js
+++ src/middleware/responseHandler.js
@@ -1,3 +1,3 @@
 export function responseHandler(req, res) {
-   res.status(200).send({ status: true, resource: res.locals.resource.subject });
+   res.status(200).json({ status: true, resource: res.locals.resource.subject });
 }
