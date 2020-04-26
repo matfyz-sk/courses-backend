8a50e35e97442e50c58657a32526457619e316c6
Milan Cifra <milan3362@gmail.com>
2020-04-26 15:25:56 +0200

    prepare resource

--- src/middleware/prepareResource.js
+++ src/middleware/prepareResource.js
@@ -3,12 +3,11 @@ import { getResourceObject } from "../helpers";
 
 export function prepareResource(req, res, next) {
    try {
-      const resource = new Resource({
+      res.locals.resource = new Resource({
          resource: getResourceObject(req.params.className),
          user: req.user,
          id: req.params.id,
       });
-      res.locals.resource = resource;
       next();
    } catch (err) {
       next(err);
