930410738fab927212bc41b94c4a8627a39d931f
Milan Cifra <milan3362@gmail.com>
2020-05-03 20:46:13 +0200

    endpoint back to matfyz

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const authSecret = "courses";
-export const virtuosoEndpoint = "http://127.0.0.1:8890/sparql";
+export const virtuosoEndpoint = "http://matfyz.sk:8890/sparql";
 export const dcTermsURI = "http://purl.org/dc/terms/";
 export const ontologyURI = "http://www.courses.matfyz.sk/ontology#";
 export const graphURI = "http://www.courses.matfyz.sk/data";
