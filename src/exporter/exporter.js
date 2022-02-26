0987254d6576d8c4b85cb7edd16d7a80da482ef0
bl96 <47901930+bl96@users.noreply.github.com>
2022-02-26 21:24:19 +0100

    Update exporter.js

--- src/exporter/exporter.js
+++ src/exporter/exporter.js
@@ -1,16 +1,33 @@
-//import * as models from "../model/index.js";
-let models = require("../model/index.js");
+import * as models from "../model/index.js";
 
-const DATA_PREFIX = "";
-const ONTOLOGY_PREFIX = "";
+const PREFIXES = {
+    courses: {prefix: "courses", uri: "http://www.courses.matfyz.sk/ontology#"},
+    coursesData: {prefix: "courses-data", uri: "http://www.courses.matfyz.sk/data/"},
+    rdfs: {prefix: "rdfs", uri: "http://www.w3.org/2000/01/rdf-schema#"}
+};
+
+const RDFS_TYPE = "a";
+
+//npm install && npm install -g babel-cli
+//npx babel-node src/exporter/exporter.js
+
+function firstLetterToUppercase(value) {
+    return value && value.length > 0 ? (value[0].toUpperCase() + value.slice(1)) : value;
+}
 
 function exportOntology() {
-    console.log("here");
-    let ontology = [];
-    for (let model in models) {
-        console.log(model.type);
-    }
-    return ontology;
+
+    Object.values(PREFIXES).map((prefixItem) => {
+        console.log("PREFIX " + prefixItem.prefix + ": <" + prefixItem.uri + ">")
+    })
+
+    console.log("\n");
+
+    Object.values(models).map((model) => {
+        if (model.type) {
+            console.log(PREFIXES.courses.prefix + ":" + firstLetterToUppercase(model.type) + " " + RDFS_TYPE + " " + PREFIXES.rdfs.prefix + ":Class");
+        }
+    })
 }
 
 exportOntology();
