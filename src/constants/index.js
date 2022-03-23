382b542e9a27bc05b70496af1f8cfabee600d256
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2022-03-23 12:02:49 +0100

    Use nullish coalescing operator for environment-based constats

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-export const AUTH_SECRET = process.env.AUTH_SECRET ? process.env.AUTH_SECRET : "courses";
-export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ? process.env.SPARQL_ENDPOINT : "http://matfyz.sk:8890/sparql";
+export const AUTH_SECRET = process.env.AUTH_SECRET ?? "courses";
+export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ?? "http://matfyz.sk:8890/sparql";
 export const DC_TERMS_URI = "http://purl.org/dc/terms/";
-export const DEFAULT_URI = process.env.DEFAULT_URI ? process.env.DEFAULT_URI : "http://www.courses.matfyz.sk/";
-export const GRAPH_NAME = process.env.GRAPH_NAME ? process.env.GRAPH_NAME : DEFAULT_URI;
+export const DEFAULT_URI = process.env.DEFAULT_URI ?? "http://www.courses.matfyz.sk/";
+export const GRAPH_NAME = process.env.GRAPH_NAME ?? DEFAULT_URI;
 export const ONTOLOGY_URI = DEFAULT_URI + "ontology#";
 export const DATA_URI = DEFAULT_URI + "data";
