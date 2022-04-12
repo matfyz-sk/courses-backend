536e94e402f84db81e385aaaaa08dfb8d82dff8d
Rasto <raleu1@gmail.com>
2022-04-12 22:22:38 +0200

    server limit extended

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ import { logger } from "./middleware/logger";
 const app = express();
 const port = 3010;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(logger);
 app.use("/data", dataRouter);
