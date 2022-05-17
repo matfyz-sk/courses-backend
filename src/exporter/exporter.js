93fb9ef66aade61e2e7bde3697a443e656175c0c
bl96 <47901930+bl96@users.noreply.github.com>
2022-05-17 22:33:20 +0200

    Updated export type as array

--- src/exporter/exporter.js
+++ src/exporter/exporter.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import * as models from "../model";
-import { DATA_IRI, ONTOLOGY_IRI } from "../constants";
+import {DATA_IRI, ONTOLOGY_IRI} from "../constants";
+import _ from "lodash";
 
 export const PREFIXES = {
     courses: ONTOLOGY_IRI,
@@ -17,7 +18,7 @@ const ADMIN_IDENTIFIER = "s3MzY";
 export class Exporter {
 
     constructor() {
-        if(this.constructor === Exporter) {
+        if (this.constructor === Exporter) {
             throw new Error("Abstract classes can't be instantiated.");
         }
     }
@@ -53,27 +54,28 @@ export class Exporter {
 
         Object.values(models).map((model) => {
             let className;
-            if(model.type) {
+            if (model.type) {
                 className = this.firstLetterToUppercase(model.type);
+                console.log("MOOOOODEL", model.type, className);
                 ontologyArray.push(this.getTriple(PREFIXES.courses, className, PREFIXES.rdf, "type", PREFIXES.rdfs, "Class"));
             }
-            if(model.subclassOf && model.subclassOf.type) {
+            if (model.subclassOf && model.subclassOf.type) {
                 ontologyArray.push(this.getTriple(PREFIXES.courses, className, PREFIXES.rdfs, "subClassOf", PREFIXES.courses, this.firstLetterToUppercase(model.subclassOf.type)));
             }
 
-            if(model.subclasses) {
-                for(let subclass of model.subclasses) {
+            if (model.subclasses) {
+                for (let subclass of model.subclasses) {
                     ontologyArray.push(this.getTriple(PREFIXES.courses, this.firstLetterToUppercase(subclass), PREFIXES.rdfs, "subClassOf", PREFIXES.courses, className));
                 }
             }
-            if(model.props) {
-                Object.entries(model.props).map(([ propertyName, propertyObject ]) => {
+            if (model.props) {
+                Object.entries(model.props).map(([propertyName, propertyObject]) => {
                     ontologyArray.push(this.getTriple(PREFIXES.courses, propertyName, PREFIXES.schema, "domainIncludes", PREFIXES.courses, className));
-                    if(propertyObject) {
-                        if(propertyObject.objectClass) {
+                    if (propertyObject) {
+                        if (propertyObject.objectClass) {
                             ontologyArray.push(this.getTriple(PREFIXES.courses, propertyName, PREFIXES.schema, "rangeIncludes", PREFIXES.courses, this.firstLetterToUppercase(propertyObject.objectClass)));
                         }
-                        if(propertyObject.dataType) {
+                        if (propertyObject.dataType) {
                             ontologyArray.push(this.getTriple(PREFIXES.courses, propertyName, PREFIXES.rdf, "type", PREFIXES.owl, this.getTypeOfProperty(propertyObject.dataType)));
                         }
                     }
@@ -91,7 +93,7 @@ export class Exporter {
 
         userArray.push(this.getUserTypeTriple());
 
-        Object.entries(adminSettings).map(([ fieldName, fieldValue ]) => {
+        Object.entries(adminSettings).map(([fieldName, fieldValue]) => {
             userArray.push(this.getAdminTriple(userIri, fieldName, fieldValue));
         });
         return userArray;
@@ -103,14 +105,24 @@ export class Exporter {
 
     //string, datetime, boolean, float, integer, node
     getTypeOfProperty(dataType) {
-        if(dataType === "node") {
+        if (dataType === "node") {
             return "ObjectProperty";
         }
         return "DatatypeProperty";
     }
 
     firstLetterToUppercase(value) {
-        return value && value.length > 0 ? (value[0].toUpperCase() + value.slice(1)) : value;
+        if (value.length < 1) {
+            return value;
+        }
+        
+        let val;
+        if (_.isArray(value)) {
+            val = value[0];
+        } else {
+            val = value;
+        }
+        return val[0].toUpperCase() + val.slice(1);
     }
 
     getTriple(sprefix, s, pprefix, p, oprefix, o) {
