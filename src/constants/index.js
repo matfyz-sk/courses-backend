d22d17a83544539c8f486adccb31779b11d5b677
bl96 <47901930+bl96@users.noreply.github.com>
2022-10-29 18:13:47 +0200

    Reverted constants to previous

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const AUTH_SECRET = process.env.AUTH_SECRET ?? "courses";
-export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ?? "http://127.0.0.1:8890/sparql";
+export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ?? "http://matfyz.sk:8890/sparql";
 export const DC_TERMS_IRI = "http://purl.org/dc/terms/";
-export const BASE_IRI = process.env.BASE_IRI ?? "http://www.testing.courses.matfyz.sk/";
+export const BASE_IRI = process.env.BASE_IRI ?? "http://www.courses.matfyz.sk/";
 export const GRAPH_IRI = process.env.GRAPH_IRI ?? BASE_IRI;
 export const ONTOLOGY_IRI = BASE_IRI + "ontology#";
 export const DATA_IRI = BASE_IRI + "data";
