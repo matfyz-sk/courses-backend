8a33272bfd01ed1db7ce7b40754383e846875928
bl96 <47901930+bl96@users.noreply.github.com>
2023-01-23 19:50:44 +0100

    Added ultragraphql build, config, and command to run it at server start

--- src/server.js
+++ src/server.js
@@ -4,16 +4,17 @@ import cors from "cors";
 import chalk from "chalk";
 import dataRouter from "./routes/data";
 import authRouter from "./routes/auth";
-import { errorHandler } from "./middleware";
-import { dateTime } from "./helpers";
-import { logger } from "./middleware/logger";
-import { ExporterSparql } from "./exporter/exporter-sparql";
+import {errorHandler} from "./middleware";
+import {dateTime} from "./helpers";
+import {logger} from "./middleware/logger";
+import {ExporterSparql} from "./exporter/exporter-sparql";
+import {exec} from 'child_process';
 
 const app = express();
 const port = 3010;
 
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
+app.use(bodyParser.json({limit: "50mb"}));
 app.use(cors());
 app.use(logger);
 app.use("/data", dataRouter);
@@ -21,9 +22,16 @@ app.use("/auth", authRouter);
 app.use(errorHandler);
 
 app.listen(port, () => {
-        console.log(chalk.green(`[${ dateTime() }]`), `Server running on port ${ port }`);
+        console.log(chalk.green(`[${dateTime()}]`), `Server running on port ${port}`);
         let exporterSparql = new ExporterSparql();
         exporterSparql.exportOntology();
+
+        const ultraGraphQLCommand = 'java -jar ./src/ultragraphql/ultragraphql-1.1.4-exe.jar --config ./src/ultragraphql/config.json';
+        exec(ultraGraphQLCommand, function (error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
+        });
     }
 )
 
