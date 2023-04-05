c1b460a34c197706655d543057344344309ee8ce
bl96 <47901930+bl96@users.noreply.github.com>
2023-04-05 14:57:49 +0200

    Changed port in constants to 8890

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const AUTH_SECRET = process.env.AUTH_SECRET ?? "courses";
-export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ?? "http://127.0.0.1:8880/sparql";
+export const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT ?? "http://127.0.0.1:8890/sparql";
 export const DC_TERMS_IRI = "http://purl.org/dc/terms/";
 export const BASE_IRI = process.env.BASE_IRI ?? "http://www.courses.matfyz.sk/";
 export const GRAPH_IRI = process.env.GRAPH_IRI ?? BASE_IRI;
